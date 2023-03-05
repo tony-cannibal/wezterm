05566e0b0908901babd038131c26e25c0fc87d2f
tony-cannibal <castrovital.luis@gmail.com>
2023-03-04 20:43:27 -0600

    Added Some More Options

--- wezterm.lua
+++ wezterm.lua
@@ -1,25 +1,37 @@
 local wezterm = require("wezterm")
 
-local my_font = wezterm.font {
-	family = "JetBrainsMono Nerd Font",
-	weight = "Regular",
-	bold = false,
-	italic = false,
-	font_size = 9,
+local my_fonts = {
+    wezterm.font {
+        family = "JetBrainsMono Nerd Font",
+        weight = "Regular",
+        bold = false,
+        italic = false,
+        -- font_size = 8,
+    },
+    wezterm.font {
+        family = "rissole",
+        weight = "Regular",
+        bold = false,
+        italic = false,
+        -- font_size = 10,
+    },
 }
 
 local colors = {
     "Green Screen (base16)",
-    "Gruvbox dark, hard (base16)",
-    "Gruvbox dark, medium (base16)"
+    "Gruvbox dark, medium (base16)",
+    "Everforest",
 }
 
 return {
 	hide_tab_bar_if_only_one_tab = true,
+    window_close_confirmation = 'NeverPrompt',
 	freetype_interpreter_version = 38,
 	-- freetype_load_flags = 'DEFAULT',
 	freetype_load_flags = "FORCE_AUTOHINT",
-	font = my_font,
-	font_size = 9,
+	font = my_fonts[1],
+	font_size = 8,
+    color_scheme_dirs = { '/home/luis/.config/wezterm/colors/' },
 	color_scheme = colors[3],
+    window_background_opacity = 0.8,
 }
